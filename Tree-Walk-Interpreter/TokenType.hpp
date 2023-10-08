611fca41041875f8d0f8ec6a42a81f601eb75705
Victor Miguel <vmmc2@cin.ufpe.br>
2023-10-07 23:40:58 -0300

    Add update related to TokenType from Chapter 4.

---
+++ Tree-Walk-Interpreter/TokenType.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+
+enum TokenType{
+    // Single-Character Tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, PLUS, MINUS, STAR, SLASH, SEMICOLON
+
+    // One or Two Character Tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE
+
+    // End of File
+    EOF
+};
+
+std::string toString(TokenType type){
+    static const std::string strings[] = {
+        "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE", "RIGHT_BRACE",
+        "COMMA", "DOT", "PLUS", "MINUS", "STAR", "SLASH", "SEMICOLON",
+        "BANG", "BANG_EQUAL", "EQUAL", "EQUAL_EQUAL",
+        "GREATER", "GREATER_EQUAL", "LESS", "LESS_EQUAL",
+        "IDENTIFIER", "STRING", "NUMBER",
+        "AND", "CLASS", "ELSE", "FALSE", "FUN", "FOR", "IF", "NIL", "OR",
+        "PRINT", "RETURN", "SUPER", "THIS", "TRUE", "VAR", "WHILE",
+        "EOF"
+    };
+
+    return strings[static_cast<int>(type)];
+}
+
