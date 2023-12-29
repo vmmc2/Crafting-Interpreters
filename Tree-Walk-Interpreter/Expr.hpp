f2dcdf13e932c53fd5d4cbafd82ebd5a57739550
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2023-12-28 22:36:53 -0300

    Update Expr.hpp

--- Tree-Walk-Interpreter/Expr.hpp
+++ Tree-Walk-Interpreter/Expr.hpp
@@ -13,6 +13,13 @@ struct Unary;
 struct Literal;
 struct Grouping;
 
+struct ExprVisitor{
+    virtual std::any visitBinaryExpr(std::shared_ptr<Binary> expr) = 0;
+    virtual std::any visitUnaryExpr(std::shared_ptr<Unary> expr) = 0;
+    virtual std::any visitLiteralExpr(std::shared_ptr<Literal> expr) = 0;
+    virtual std::any visitGroupingExpr(std::shared_ptr<Grouping> expr) = 0;
+    virtual ~ExprVisitor() = default;
+};
 
 struct Expr{
     virtual std::any accept(ExprVisitor& visitor) = 0;
@@ -27,6 +34,9 @@ struct Binary : Expr, public std::enable_shared_from_this<Binary>{
         : left{std::move(left)}, op{std::move(op)}, right{std::move(right)}
     {}
 
+    std::any accept(ExprVisitor& visitor) override{
+        return visitor.visitBinaryExpr(shared_from_this());
+    }
 };
 
 struct Unary : Expr, public std::enable_shared_from_this<Unary>{
@@ -36,6 +46,10 @@ struct Unary : Expr, public std::enable_shared_from_this<Unary>{
     Unary(Token op, std::shared_ptr<Expr> right)
         : op{std::move(op)}, right{std::move(right)} 
     {}
+
+    std::any accept(ExprVisitor& visitor) override{
+        return visitor.visitUnaryExpr(shared_from_this());
+    }
 };
 
 struct Literal : Expr, public std::enable_shared_from_this<Literal>{
@@ -44,6 +58,10 @@ struct Literal : Expr, public std::enable_shared_from_this<Literal>{
     Literal(std::any value)
         : value{std::move(value)}
     {}
+
+    std::any accept(ExprVisitor& visitor) override{
+        return visitor.visitLiteralExpr(shared_from_this());
+    }
 };
 
 struct Grouping : Expr, std::enable_shared_from_this<Grouping>{
@@ -52,4 +70,8 @@ struct Grouping : Expr, std::enable_shared_from_this<Grouping>{
     Grouping(std::shared_ptr<Expr> expression)
         : expression{std::move(expression)}
     {}
+
+    std::any accept(ExprVisitor& visitor) override{
+        return visitor.visitGroupingExpr(shared_from_this());
+    }
 };
