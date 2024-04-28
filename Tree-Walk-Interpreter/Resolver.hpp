ec74485d11ee4175a3b86d1ccaa0e9a5d160fbad
Victor Miguel <vmmc5910@gmail.com>
2024-04-27 22:42:18 -0300

    Add code related to chapter 11.

--- Tree-Walk-Interpreter/Resolver.hpp
+++ Tree-Walk-Interpreter/Resolver.hpp
@@ -26,6 +26,29 @@ class Resolver : public ExprVisitor, public StmtVisitor{
       return;
     }
 
+    void resolveFunction(std::shared_ptr<Function> function){
+      beginScope();
+      for(const Token& param : function->parameters){
+        declare(param);
+        define(param);
+      }
+      resolve(function->body);
+      endScope();
+
+      return;
+    }
+
+    void resolveLocal(std::shared_ptr<Expr> expr, const Token& name){
+      for(int i = scopes.size() - 1; i >= 0 ; i--){
+        if(scopes[i].find(name.lexeme) != scopes[i].end()){
+          interpreter.resolve(expr, scopes.size() - 1 - i);
+          return;
+        }
+      }
+
+      return;
+    }
+
     void beginScope(){
       scopes.push_back(std::map<std::string, bool>{});
 
@@ -34,6 +57,23 @@ class Resolver : public ExprVisitor, public StmtVisitor{
 
     void endScope(){
       scopes.pop_back();
+
+      return;
+    }
+
+    void declare(const Token& name){
+      if(scopes.empty()) return;
+
+      std::map<std::string, bool>& scope = scopes.back();
+      scope[name.lexeme] = false;
+
+      return;
+    }
+
+    void define(const Token& name){
+      if(scopes.empty()) return;
+
+      scopes.back()[name.lexeme] = true;
 
       return;
     }
@@ -61,12 +101,115 @@ class Resolver : public ExprVisitor, public StmtVisitor{
       return {};
     }
 
+    std::any visitExpressionStmt(std::shared_ptr<Expression> stmt) override{
+      resolve(stmt->expression);
+
+      return {};
+    }
+
+    std::any visitFunctionStmt(std::shared_ptr<Function> stmt) override{
+      declare(stmt->name);
+      define(stmt->name);
+
+      resolveFunction(stmt);
+      return {};
+    }
+
+    std::any visitIfStmt(std::shared_ptr<If> stmt) override{
+      resolve(stmt->condition);
+      resolve(stmt->ifBranch);
+
+      if(stmt->elseBranch != nullptr){
+        resolve(stmt->elseBranch);
+      }
+
+      return {};
+    }
+
+    std::any visitPrintStmt(std::shared_ptr<Print> stmt) override{
+      resolve(stmt->expression);
+
+      return {};
+    }
+
+    std::any visitReturnStmt(std::shared_ptr<Return> stmt) override{
+      if(stmt->value != nullptr){
+        resolve(stmt->value);
+      }
+
+      return {};
+    }
+
     std::any visitVarStmt(std::shared_ptr<Var> stmt) override{
       declare(stmt->name);
       if(stmt->initializer != nullptr){
         resolve(stmt->initializer);
       }
       define(stmt->name);
+
+      return {};
+    }
+
+    std::any visitWhileStmt(std::shared_ptr<While> stmt) override{
+      resolve(stmt->condition);
+      resolve(stmt->body);
+
+      return {};
+    }
+
+
+    std::any visitAssignExpr(std::shared_ptr<Assign> expr) override{
+      resolve(expr->value);
+      resolveLocal(expr, expr->name);
+
+      return {};
+    }
+
+    std::any visitBinaryExpr(std::shared_ptr<Binary> expr) override{
+      resolve(expr->left);
+      resolve(expr->right);
+
+      return {};
+    }
+
+    std::any visitCallExpr(std::shared_ptr<Call> expr) override{
+      resolve(expr->callee);
+
+      for(const std::shared_ptr<Expr>& argument : expr->arguments){
+        resolve(argument);
+      }
+
+      return {};
+    }
+
+    std::any visitGroupingExpr(std::shared_ptr<Grouping> expr) override{
+      resolve(expr->expression);
+
+      return {};
+    }
+
+    std::any visitLiteralExpr(std::shared_ptr<Literal> expr) override{
+      return {};
+    }
+
+    std::any visitLogicalExpr(std::shared_ptr<Logical> expr) override{
+      resolve(expr->left);
+      resolve(expr->right);
+
+      return {};
+    }
+
+    std::any visitUnaryExpr(std::shared_ptr<Unary> expr) override{
+      resolve(expr->right);
+      
+      return {};
+    }
+
+    std::any visitVariableExpr(std::shared_ptr<Variable> expr) override{
+      if(!scopes.empty() && scopes.back()[expr->name.lexeme] == false){
+        error(expr->name, "Can't read local variable in its own initializer.");
+      }
+      resolveLocal(expr, expr->name);
 
       return {};
     }
