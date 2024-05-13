ca1bebeab563e565931f09067ca406c310bf753a
Victor Miguel <vmmc5910@gmail.com>
2024-05-12 21:44:32 -0300

    Add more code related to Chapter 12.

--- Tree-Walk-Interpreter/LoxFunction.cpp
+++ Tree-Walk-Interpreter/LoxFunction.cpp
@@ -5,8 +5,8 @@
 #include "Interpreter.hpp"
 #include "LoxFunction.hpp"
 
-LoxFunction::LoxFunction(std::shared_ptr<Function> declaration, std::shared_ptr<Environment> closure)
-  : declaration{std::move(declaration)}, closure{std::move(closure)}
+LoxFunction::LoxFunction(std::shared_ptr<Function> declaration, std::shared_ptr<Environment> closure, bool isInitializer)
+  : declaration{std::move(declaration)}, closure{std::move(closure)}, isInitializer{isInitializer}
 {}
 
 std::string LoxFunction::toString(){
@@ -35,6 +35,10 @@ std::any LoxFunction::call(Interpreter& interpreter, std::vector<std::any> argum
     interpreter.executeBlock(declaration->body, environment); // Execute the body of the funtion by passing its statements and its current environment.
   }catch(LoxReturn returnValue){
     return returnValue.value;
+  }
+
+  if(isInitializer){
+    return closure->getAt(0, "this");
   }
 
   return nullptr; // Automatically deals with the case where there is no 'return' statement in the body of the function. By default, in these cases, Lox functions return nil.
--- Tree-Walk-Interpreter/LoxFunction.hpp
+++ Tree-Walk-Interpreter/LoxFunction.hpp
@@ -13,11 +13,12 @@ class LoxInstance;
 
 class LoxFunction : public LoxCallable{
   private:
+    bool isInitializer;
     std::shared_ptr<Function> declaration;
     std::shared_ptr<Environment> closure;
 
   public:
-    LoxFunction(std::shared_ptr<Function> declaration, std::shared_ptr<Environment> closure);
+    LoxFunction(std::shared_ptr<Function> declaration, std::shared_ptr<Environment> closure, bool isInitializer);
     int arity() override;
     std::shared_ptr<LoxFunction> bind(std::shared_ptr<LoxInstance> instance);
     std::any call(Interpreter& interpreter, std::vector<std::any> arguments) override;
